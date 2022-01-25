24baf0e5dfb390fae6042dafd4e8810d0d6e09ee
Liubov kolotyrina <kolotyrina.l@yandex.ru>
2022-01-25 20:40:20 +0300

    hw1

--- task_2/solution.js
+++ task_2/solution.js
@@ -4,19 +4,16 @@ function calcShipping(sum, min, shipping) {
     let shippingPrice = shipping; // стоимость доставки
 
     // Задание №2.1. Рассчитать доставку
-
-    // создайте переменную shippingSum
-
-    // если productsSum равно 0,
-    // то shippingSum присвоить значение 0
-
-    // если productsSum Больше или равна freeShippingMinSum,
-    // то shippingSum присвоить значение 0
-
-    // если productsSum больше 0 и меньше freeShippingMinSum,
-    // то shippingSum присвоить значение shippingPrice
-
-    // Конец решения задания №2.1.
+    let shippingSum;
+    if (productsSum == 0) {
+        shippingSum = 0;
+    }
+    if (productsSum >= freeShippingMinSum) {
+        shippingSum = 0;
+    }
+    if ((productsSum > 0) && (productsSum < freeShippingMinSum)) {
+     shippingSum = shippingPrice;
+    }
 
     return shippingSum;
 }
@@ -27,9 +24,13 @@ function calcDiscount(sum, min, discount) {
     let discountPart = discount; // величина скидки в процентах
 
     // Задание №2.2. Рассчитать скидку
-
+    let discountSum;
     // создайте переменную discountSum
-
+    if productsSum >= discountMinSum{
+        discountSum = discountPart / 100 * productsSum;
+    } else {
+        discountSum = 0;
+    }
     // если productsSum больше или равно discountMinSum,
     // то присвойте discountSum значение discountPart процентов от productsSum,
     // иначе присвойте discountSum значение 0
